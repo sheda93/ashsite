6fe46e372d13152a020514374ae299a469a6e83a
sheda93 <41135887+sheda93@users.noreply.github.com>
2018-07-25 22:54:34 -0400

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+if(isset($_POST['submit'])){
+    //$to = "sheenad18@gmail.com"; // this is your Email address
+    //$from = $_POST['emailaddress']; // this is the sender's Email address
+    //$name = "Name" . $_POST['name'];
+    //$event = "Type of Event" . $_POST['event'];
+    //$date = $_POST['date'];
+    //$subject = "New Booking Request";
+    //$details = "Additional Details:" . "\n\n" . $_POST['details'];
+   // $headers = "From:" . $from;
+    //mail($to,$subject,$details,$headers);
+    //echo "Thank you " . $name . "! Your request has been received. You will be contacted you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    echo "Got your info, thanks!";
+}
+?>
